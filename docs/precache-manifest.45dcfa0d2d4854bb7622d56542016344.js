e6afbeecf5eaf253811103ed38424b5cf829491a
Ruslan Snover <reply.snover@gmail.com>
2019-08-01 19:12:28 +0300

    Production folder

---
+++ docs/precache-manifest.45dcfa0d2d4854bb7622d56542016344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e4675e0ad71b4384f3ce4d9bc4095f",
+    "url": "index.html"
+  },
+  {
+    "revision": "6cabb7f01916a51402e5",
+    "url": "static/css/main.7aebbb44.chunk.css"
+  },
+  {
+    "revision": "a09c77e1fc2f12f8609f",
+    "url": "static/js/2.62d843bb.chunk.js"
+  },
+  {
+    "revision": "6cabb7f01916a51402e5",
+    "url": "static/js/main.55b94792.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "static/js/runtime~main.a3a648b2.js"
+  },
+  {
+    "revision": "54335fd02372f8eba1276f8a4c6a0ef7",
+    "url": "static/media/sad_dog.54335fd0.png"
+  }
+]);
