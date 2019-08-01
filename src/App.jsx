07a50732d87b4e9bbdb6f2985bd0ef9f5bd99bf7
Ruslan Snover <reply.snover@gmail.com>
2019-08-01 19:28:16 +0300

    App component

---
+++ src/App.jsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react';
+import './app.css';
+import InputField from './components/input-field/input-field';
+import TaskList from './components/task-list/task-list';
+import MadeWithLove from './components/footer/with-love';
+
+
+const App = () => {
+    const [tasks, setTasks] = useState([]),
+        githubLink = "https://github.com/SnovRuslan";
+
+    /**
+     * When App component will mounted.
+     * useEffect callback will check
+     * if any tasks are already in localStorage.
+     */
+    useEffect(() => {
+        if (localStorage.getItem('-app-tasks')) {
+            const tasksFromStorage = localStorage.getItem('-app-tasks').split(',');
+            setTasks(tasksFromStorage);
+        }
+    }, []);
+
+    /**
+     * Submitting user task by clicking Enter.
+     * First checks if task with the same name alreay exist.
+     * And then ckecing if the input value isn't just a spaces.
+     * If everything all right - adding task to list
+     * and renewing component state
+     */
+    const submitUserTask = e => {
+        if (e.key === 'Enter') {
+            // checking if the same task already added.
+            const hasSameTask = tasks.some(item => {
+                return item.toLowerCase() === e.target.value.trim().toLowerCase();
+            });
+
+            if (hasSameTask) {
+                e.target.style.outlineColor = 'var(--red)';
+                alert("The same task already in a list. Try again!");
+                return;
+            }
+
+            // checking correctivity and then submitting a task.
+            if (!e.target.value.trim()) {
+                e.target.style.outlineColor = 'var(--red)';
+            }
+            else if (e.target.value.trim()) {
+                const newTasks = [...tasks, e.target.value.trim()];
+                setTasks(newTasks);
+                localStorage.setItem('-app-tasks', newTasks);
+
+                e.target.style.outlineColor = 'var(--green)';
+                e.target.value = '';
+            }
+        }
+    };
+
+    /**
+     * Deletes user task after clicking delete icon.
+     * Filtering tasks in state and then renewing state.
+     */
+    const deleteUserTask = e => {
+        const filtered = tasks.filter(item => {
+            return item !== e.target.parentNode.firstChild.textContent;
+        });
+
+        setTasks(filtered);
+        localStorage.setItem('-app-tasks', filtered);
+    };
+
+    /**
+     * Editting user task after clicking edit icon.
+     * Makes <div> with task text content editable
+     * and adds eventListener to it (keydown).
+     * if new task will be correct, it renews tasks in state
+     */
+    const editUserTask = e => {
+        const taskBlock = e.target.parentNode.firstChild,
+            previousBlockValue = taskBlock.textContent;
+
+        taskBlock.contentEditable = true;
+        taskBlock.focus();
+        taskBlock.title = "Click enter to submit the task";
+
+        taskBlock.addEventListener('keydown', e => {
+            if (e.key === 'Enter' && e.target.textContent.trim()) {
+                taskBlock.contentEditable = false;
+                taskBlock.title = "Double click to mark as done";
+
+                tasks.map((item, idx) => {
+                    if (item === previousBlockValue) {
+                        tasks[idx] = e.target.textContent.trim();
+                    }
+                    return true;
+                });
+
+                setTasks(tasks);
+                localStorage.setItem('-app-tasks', tasks);
+            }
+        });
+    };
+
+    return (
+        <>
+            <header title="Simple TODO app made with React.js v.16.8">
+                <a
+                    href={`${githubLink}/react-todo-app`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >TODO</a>
+            </header>
+
+            <div className="wrapper">
+                <InputField submitTask={submitUserTask} />
+                <TaskList
+                    list={tasks}
+                    removeTask={deleteUserTask}
+                    editTask={editUserTask}
+                />
+            </div>
+
+            <footer>
+                <MadeWithLove githubProfile={githubLink} />
+            </footer>
+        </>
+    );
+};
+
+export default App;
