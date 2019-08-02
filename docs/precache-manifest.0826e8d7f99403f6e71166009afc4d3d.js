22b4e32fc96ed3aff668593e9c5345bb03d68ae1
Ruslan Snover <reply.snover@gmail.com>
2019-08-03 01:04:24 +0300

    Adaptive in production

--- docs/precache-manifest.0826e8d7f99403f6e71166009afc4d3d.js
+++ docs/precache-manifest.0826e8d7f99403f6e71166009afc4d3d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "853932be01fec9b6bfa40dbe35efdbd1",
+    "revision": "c46c2593d1c933f44872461f4ffc3833",
     "url": "index.html"
   },
   {
-    "revision": "495005dbc1343fd4180b",
-    "url": "static/css/main.7aebbb44.chunk.css"
+    "revision": "e873685904e7661d4189",
+    "url": "static/css/main.70633dcf.chunk.css"
   },
   {
     "revision": "e5a754383f15f4d6d10a",
     "url": "static/js/2.0f345ad2.chunk.js"
   },
   {
-    "revision": "495005dbc1343fd4180b",
-    "url": "static/js/main.3d20f840.chunk.js"
+    "revision": "e873685904e7661d4189",
+    "url": "static/js/main.fda85c67.chunk.js"
   },
   {
     "revision": "b1aeed9f6344163e5e40",
