5a6b830ba4d614b9f52725eef21245987f37ba95
Ruslan Snover <reply.snover@gmail.com>
2019-08-03 01:12:22 +0300

    Audits error fixed: only li elements must be in a list

--- src/components/task-list/task-list.jsx
+++ src/components/task-list/task-list.jsx
@@ -6,20 +6,22 @@ import NoTaskBlock from './no-task/no-task-block';
 
 const TaskList = ({ list, removeTask, editTask, markAsDone }) => {
     return (
-        <ul className="task-list">
-            {
-                list.map((task, idx) => (
-                    <Task
-                        key={idx}
-                        task={task}
-                        removeTask={removeTask}
-                        editTask={editTask}
-                        markAsDone={markAsDone}
-                    />
-                ))
-            }
+        <>
+            <ul className="task-list">
+                {
+                    list.map((task, idx) => (
+                        <Task
+                            key={idx}
+                            task={task}
+                            removeTask={removeTask}
+                            editTask={editTask}
+                            markAsDone={markAsDone}
+                        />
+                    ))
+                }
+            </ul>
             { list.length === 0 && <NoTaskBlock /> }
-        </ul>
+        </>
     );
 };
 
